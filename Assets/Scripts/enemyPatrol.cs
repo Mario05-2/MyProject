9d7fd2395f140ce7bf029831440df02898f6bab6
Stephen McCaskie <McCaskie-S1@ulster.ac.uk>
2024-10-22 15:51:53 +0100

    Slime Patrol and more added

--- Assets/Scripts/enemyPatrol.cs
+++ Assets/Scripts/enemyPatrol.cs
@@ -1,14 +1,16 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class enemyPatrol : MonoBehaviour
+public class EnemyPatrol : MonoBehaviour
 {
     public GameObject pointA;
     public GameObject pointB;
     private Rigidbody2D rb; 
     private Animator anim;
-    private Transform  currentPoint;
+    private Transform currentPoint;
+    public float speed;
+    private bool isIdle = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +23,53 @@ public class enemyPatrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isIdle) return;
+
+        Vector2 point = currentPoint.position - transform.position;
+        if (currentPoint == pointB.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f)
+        {
+            StartCoroutine(IdleCoroutine());
+        }
+
+        anim.SetBool("isRunning?", rb.velocity != Vector2.zero);
+    }
+
+    private IEnumerator IdleCoroutine()
+    {
+        isIdle = true;
+        rb.velocity = Vector2.zero;
+        anim.SetBool("Moving", false);
+
+        yield return new WaitForSeconds(2);
+
+        if (currentPoint == pointB.transform)
+        {
+            Flip();
+            currentPoint = pointA.transform;
+        }
+        else
+        {
+            Flip();
+            currentPoint = pointB.transform;
+        }
+
+        anim.SetBool("Moving", true);
+        isIdle = false;
+    }
+
+    public void Flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
     }
 }
